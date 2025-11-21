a76c4560e3edc98f1959117a6c189a7b6fe92e84
Christopher McLeish <themcleish@outlook.com>
2025-11-22 09:24:18 +1000

    Initial Spring Boot empty project - created with Spring Initializr.

---
+++ src/test/java/com/cat/robot/RobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
